4a3e9673e22343c08e6f60104a29c8591ae97070
“zrittawat” <tiees288@gmail.com>
2022-12-14 10:14:02 +0700

    Example Yup Validate

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,54 @@
 <template>
-  <div class="mt-12">name: {{ name }}</div>
-  <div class="mt-12">Message: {{ helloWorld() }}</div>
-  <input name="firstname" v-model="formData.firstname" />
+  <div class="mx-12 md:mx-[300px] mt-12">
+    <form @submit="onSubmit" ref="refForm" :validation-schema="schema"
+      class="flex flex-col bg-blue-400 px-12 py-4 rounded-md">
+      <h3>Login Page</h3>
+      <div class="flex flex-col">
+        <Field name="email" type="email" class="my-2 h-12 drop-shadow-md" />
+        <ErrorMessage name="email" />
+      </div>
+      <div class="flex flex-col">
+        <Field name="password" type="password" class="my-2 h-12 drop-shadow-md" />
+        <ErrorMessage name="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { Form, Field, ErrorMessage, useForm } from 'vee-validate';
+import * as yup from 'yup';
+
 interface FormData {
-  firstname: string
-  lastname: string
-}
-let formData: FormData = {
-  firstname: '',
-  lastname: ''
+  email: string
+  password: string
 }
 
-const name = '123'
-const data = useState<string>()
 const meta = useHead({
   title: `Index - Nuxt 3`
 })
 
-const helloWorld = () => {
-  return 'Hello Message'
-}
+const refForm = ref(null)
+const schema = yup.object({
+  email: yup.string().required('กรุณาระบุ Email').email(),
+  password: yup.string().required().min(8).test('Check Password Strength', 'Password เกิน 8',
+    (value: any): boolean => {
+      if (value?.toString().length > 8) {
+        return false
+      } else {
+        return true
+      }
+    }),
+  fieldArray: yup.array()
+});
+const { handleSubmit, isSubmitting } = useForm({
+  validationSchema: schema
+});
 
+const onSubmit = handleSubmit(async (values, { resetForm }) => {
+  console.log(values); // send data to API
+  // reset the form and the field values to their initial values
+  resetForm();
+});
 </script>
